28e769ece9f7831284fc1fa31edf50aca07397dc
pvblo-superior <132347051+pvblo-superior@users.noreply.github.com>
2023-05-02 17:53:40 +0800

    Add files via upload

---
+++ final lab task 1.js
@@ -0,0 +1,37 @@
+function addNumbers() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 + num2;
+    document.getElementById("result").value = result;
+}
+
+function subNumbers() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 - num2;
+    document.getElementById("result").value = result;
+}
+
+function multiplyNumbers() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 * num2;
+    document.getElementById("result").value = result;
+}
+
+function divideNumbers() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 / num2;
+    document.getElementById("result").value = result;
+}
+
+function clearValues() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("result").value = "";
+}
+function About() {
+    var fullName = "Ritzel Julie";
+    alert("Work of " + fullName);
+}
